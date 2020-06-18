cd9706487c10a45a98e5c46a88781a07f4f1cfbe
nbdy <pascal@eberlein.io>
2020-06-19 00:34:47 +0200

    update

--- shallot.py
+++ shallot.py
@@ -3,48 +3,54 @@ import re
 import sys
 import gmpy2
 import multiprocessing
+from functools import reduce
 from hashlib import sha1
 from base64 import b64encode, b32encode
 from pyasn1.codec.der import encoder
-from Queue import Empty as QueueEmpty
+from queue import Empty as QueueEmpty
 from pyasn1.type import univ, namedtype
 
-#### Constants stolen from the original shallot ####
+# Constants stolen from the original shallot ####
 EMIN = 0x10001
 EMAX = 0xFFFFFFFFFF
 
-#### Prime finding stuff for RSA ####
+
+# Prime finding stuff for RSA ####
 def random(bytez):
-    '''Produces a random number thats has bytez*8 amount of bits.'''
-    return gmpy2.mpz(reduce(lambda a, b: (a << 8)|ord(b), os.urandom(bytez), 0))
+    """Produces a random number thats has bytez*8 amount of bits."""
+    return gmpy2.mpz(reduce(lambda a, b: (a << 8) | ord(chr(b)), os.urandom(bytez), 0))
+
 
 def good_prime(p):
-    '''True if highly probably prime, else false.'''
-    return gmpy2.is_prime(p, 1000) and \
-           gmpy2.is_strong_bpsw_prp(p)
+    """True if highly probably prime, else false."""
+    return gmpy2.is_prime(p, 1000) and gmpy2.is_strong_bpsw_prp(p)
+
 
 def find_prime(bytez=128):
-    '''Checks random numbers for primality'''
-    p = random(bytez)|1
+    """Checks random numbers for primality"""
+    p = random(bytez) | 1
     while not good_prime(p):
-        p = random(bytez)|1
+        p = random(bytez) | 1
     return p
 
+
 def good_pair(p, q):
-    '''Returns p*q if p and q are a good pair, else 0.'''
-    n = p*q
+    """Returns p*q if p and q are a good pair, else 0."""
+    n = p * q
     k = gmpy2.ceil(gmpy2.log2(n))
-    if abs(p - q) > 2**(k/2 - 100):
+    if abs(p - q) > 2 ** (k / 2 - 100):
         return n
     return 0
 
-##### Encoding stuffs #####
-#https://tools.ietf.org/html/rfc3447#appendix-A.1
+
+# Encoding stuffs
+# https://tools.ietf.org/html/rfc3447#appendix-A.1
 class RSAPublicKey(univ.Sequence):
     componentType = namedtype.NamedTypes(
         namedtype.NamedType('modulus', univ.Integer()),
         namedtype.NamedType('publicExponent', univ.Integer())
-        )
+    )
+
 
 class RSAPrivateKey(univ.Sequence):
     componentType = namedtype.NamedTypes(
@@ -57,41 +63,45 @@ class RSAPrivateKey(univ.Sequence):
         namedtype.NamedType('exponent1', univ.Integer()),
         namedtype.NamedType('exponent2', univ.Integer()),
         namedtype.NamedType('coefficient', univ.Integer())
-        )
+    )
+
 
 def public_key(n, e):
-    public_key = RSAPublicKey()
-    public_key.setComponentByName('modulus', n)
-    public_key.setComponentByName('publicExponent', e)
-    return encoder.encode(public_key)
+    pk = RSAPublicKey()
+    pk.setComponentByName('modulus', n)
+    pk.setComponentByName('publicExponent', e)
+    return encoder.encode(pk)
+
 
 def make_onion(n, e):
-    return b32encode(sha1(public_key(n, e)).digest())[:16].lower()+'.onion'
+    return b32encode(sha1(public_key(n, e)).digest())[:16].lower() + b'.onion'
+
 
 def private_key(n, e, d, p, q):
-    private_key = RSAPrivateKey()
-    private_key.setComponentByName('version', 0)
-    private_key.setComponentByName('modulus', n)
-    private_key.setComponentByName('publicExponent', e)
-    private_key.setComponentByName('privateExponent', d)
-    private_key.setComponentByName('prime1', p)
-    private_key.setComponentByName('prime2', q)
-    private_key.setComponentByName('exponent1', d % (p - 1))
-    private_key.setComponentByName('exponent2', d % (q - 1))
-    private_key.setComponentByName('coefficient', gmpy2.invert(q, p))
-    return encoder.encode(private_key)
+    pk = RSAPrivateKey()
+    pk.setComponentByName('version', 0)
+    pk.setComponentByName('modulus', n)
+    pk.setComponentByName('publicExponent', e)
+    pk.setComponentByName('privateExponent', d)
+    pk.setComponentByName('prime1', p)
+    pk.setComponentByName('prime2', q)
+    pk.setComponentByName('exponent1', d % (p - 1))
+    pk.setComponentByName('exponent2', d % (q - 1))
+    pk.setComponentByName('coefficient', gmpy2.invert(q, p))
+    return encoder.encode(pk)
+
 
 def pprint_privkey(privkey):
-    print '-'*5 + 'BEGIN RSA PRIVATE KEY' + '-'*5
+    print('-' * 5 + 'BEGIN RSA PRIVATE KEY' + '-' * 5)
     encoded = b64encode(privkey)
     while encoded:
         chunk, encoded = encoded[:64], encoded[64:]
-        print chunk
-    print '-'*5 + 'END RSA PRIVATE KEY' + '-'*5
+        print(chunk)
+    print('-' * 5 + 'END RSA PRIVATE KEY' + '-' * 5)
 
-#### Worker process generates keys, hashes, and checks for patterns ####
+
+# Worker process generates keys, hashes, and checks for patterns
 class Worker(multiprocessing.Process):
-
     def __init__(self, regex, results, trials, kill, *args, **kwds):
         multiprocessing.Process.__init__(self, *args, **kwds)
         self.regex = regex
@@ -119,17 +129,18 @@ class Worker(multiprocessing.Process):
                     return
                 i += 1
                 onion = make_onion(n, e)
-                if search(onion) and gmpy2.gcd(e, tot)==1:
+                if search(onion.decode('utf-8')) and gmpy2.gcd(e, tot) == 1:
                     d = gmpy2.invert(e, tot)
                     priv = private_key(n, e, d, p, q)
-                    self.results.put(onion+priv)
+                    self.results.put(onion + priv)
                     self.trials.put(i)
                     self.kill.set()
                     return
                 e += 2
 
+
 def kill_procs(processes, results, trials, kill):
-    '''joins all processes, empties all queues, and returns sum of trials.'''
+    """joins all processes, empties all queues, and returns sum of trials."""
     if not kill.is_set():
         kill.set()
     sum_trials = 0
@@ -141,7 +152,8 @@ def kill_procs(processes, results, trials, kill):
         results.get()
     return sum_trials
 
-#### Main thread ####
+
+# Main thread
 def main(pattern):
     results = multiprocessing.Queue()
     trials = multiprocessing.Queue()
@@ -160,19 +172,19 @@ def main(pattern):
                 break
     except KeyboardInterrupt:
         sum_trials = kill_procs(processes, results, trials, kill)
-        print 'Tried', sum_trials, 'public keys before exit'
+        print('Tried', sum_trials, 'public keys before exit')
         sys.exit(1)
     sum_trials = kill_procs(processes, results, trials, kill)
     onion = found[:22]
     privkey = found[22:]
-    print '-'*64
-    print 'Found matching pattern after', sum_trials, 'tries:', onion
-    print '-'*64
+    print('-' * 64)
+    print('Found matching pattern after', sum_trials, 'tries:', onion)
+    print('-' * 64)
     pprint_privkey(privkey)
+
 
 if __name__ == '__main__':
     try:
         main(sys.argv[1])
     except KeyboardInterrupt:
         sys.exit(1)
-
